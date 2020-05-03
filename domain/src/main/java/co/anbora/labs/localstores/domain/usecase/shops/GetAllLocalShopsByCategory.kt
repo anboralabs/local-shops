f5b45d44844a8f84f22750d609463285f0322edd
Darwin Algarin <dalgarins@live.com>
2020-05-03 13:06:49 -0500

    Added code to GetAllLocalShopsByCategory UseCase

--- domain/src/main/java/co/anbora/labs/localstores/domain/usecase/shops/GetAllLocalShopsByCategory.kt
+++ domain/src/main/java/co/anbora/labs/localstores/domain/usecase/shops/GetAllLocalShopsByCategory.kt
@@ -1,4 +1,19 @@
 package co.anbora.labs.localstores.domain.usecase.shops
 
-class GetAllLocalShopsByCategory {
+import co.anbora.labs.localstores.domain.model.LocalShopBo
+import co.anbora.labs.localstores.domain.repository.IRepository
+import co.anbora.labs.localstores.domain.result.Result
+import co.anbora.labs.localstores.domain.usecase.FlowUseCase
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+
+class GetAllLocalShopsByCategory(
+    private val repository: IRepository,
+    defaultDispatcher: CoroutineDispatcher
+): FlowUseCase<GetLocalShopsByCategoryParameters, List<LocalShopBo>>(defaultDispatcher) {
+
+    override fun execute(parameters: GetLocalShopsByCategoryParameters): Flow<Result<List<LocalShopBo>>>
+            = repository.getLocalShopsByCategory(parameters.categoryId)
 }
+
+data class GetLocalShopsByCategoryParameters(val categoryId: String)
